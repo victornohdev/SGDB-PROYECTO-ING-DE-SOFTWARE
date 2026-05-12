22ed94db6a0d6837c572f1e20e32e3e155283ac5
victornohdev <victormahla@hotmail.com>
2026-05-12 11:37:27 -0500

    conectar mysql railway

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,3 +1,4 @@
+/*
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
@@ -6,6 +7,20 @@ const pool = mysql.createPool({
     password: 'Admin123',
     database: 'sgae',
     port: 3306 //puerto del server
+});
+
+module.exports = pool;
+*/
+
+
+const mysql = require('mysql2/promise');
+
+const pool = mysql.createPool({
+    host: process.env.MYSQLHOST,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
+    port: process.env.MYSQLPORT
 });
 
 module.exports = pool;
